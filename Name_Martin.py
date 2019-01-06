894a49a13986e31bef417041db22adc79d33a5f3
Shivam Prasad <shivam13juna@gmail.com>
2019-01-06 12:17:22 +0530

    Keras isn't giving reproducible results, trying Tensorflow now

--- Name_Martin.py
+++ Name_Martin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[63]:
 
 
 from __future__ import print_function
@@ -9,13 +9,14 @@ from __future__ import print_function
 from sklearn.preprocessing import OneHotEncoder
 from keras.layers.core import Dense, Activation, Dropout
 from keras.preprocessing import sequence
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.layers import Dense, Embedding, Bidirectional, Input, TimeDistributed
 from keras.layers import LSTM
-from keras.datasets import imdb
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras import optimizers
 import pandas as pd
 import numpy as np
+import sys
 import os
 
 
@@ -79,9 +80,10 @@ len_vocab = len(vocab)
 # In[8]:
 
 
-print(vocab)
-print("vocab length is ",len_vocab)
-print ("length of data_set is ",len(data_set1))
+# print(vocab)
+
+# print("vocab length is ",len_vocab)
+# print ("length of data_set is ",len(data_set1))
 
 
 # In[9]:
@@ -93,19 +95,19 @@ char_index = dict((c, i) for i, c in enumerate(vocab))
 # In[10]:
 
 
-print(char_index)
+# print(char_index)
 
 
-# In[11]:
+# In[37]:
 
 
 #train test split
-msk = np.random.rand(len(data_set1)) < 0.8
+msk = np.random.rand(len(data_set1)) < 0.9
 train = data_set1[msk]
 test = data_set1[~msk]     
 
 
-# In[12]:
+# In[38]:
 
 
 def set_flag(i):
@@ -114,7 +116,7 @@ def set_flag(i):
     return(tmp)
 
 
-# In[13]:
+# In[39]:
 
 
 set_flag(3)
@@ -122,7 +124,7 @@ set_flag(3)
 
 # #### modify the code above to also convert each index to one-hot encoded representation
 
-# In[14]:
+# In[40]:
 
 
 #take data_set upto max and truncate rest
@@ -147,7 +149,7 @@ train_X=np.asarray(train_X)
 train_Y=np.asarray(train_Y)
 
 
-# In[15]:
+# In[41]:
 
 
 test_X = []
@@ -168,36 +170,26 @@ test_X = np.asarray(test_X)
 test_Y = np.asarray(test_Y)
 
 
-# In[16]:
+# In[42]:
 
 
 print(np.asarray(test_X).shape)
 print(np.asarray(test_Y).shape)
 
 
-# In[17]:
-
-
-msk = np.random.rand(len(czech)) < 0.8
-
-
-vtrain = czech[msk]
-vtest = czech[~msk]
-
-
-# In[18]:
+# In[44]:
 
 
 vtrain_x = []
 vtrain_y = []
 
-train_name = [str(i) for i in vtrain.Name]
+train_name = [str(i) for i in czech.Name]
 for i in train_name:
     tmp = [set_flag(char_index[j]) for j in str(i.lower())]
     for k in range(0, maxlen - len(str(i))):
         tmp.append(set_flag(char_index['END']))
     vtrain_x.append(tmp)
-for i in vtrain.m_or_f:
+for i in czech.m_or_f:
     if i == 'm':
         vtrain_y.append([1,0])
     else:
@@ -206,72 +198,57 @@ vtrain_x = np.asarray(vtrain_x)
 vtrain_y = np.asarray(vtrain_y)
 
 
-# In[19]:
-
-
-vtest_x = []
-vtest_y = []
-
-train_name = [str(i) for i in vtest.Name]
-for i in train_name:
-    tmp = [set_flag(char_index[j]) for j in str(i.lower())]
-    for k in range(0, maxlen - len(str(i))):
-        tmp.append(set_flag(char_index['END']))
-    vtest_x.append(tmp)
-for i in vtest.m_or_f:
-    if i == 'm':
-        vtest_y.append([1,0])
-    else:
-        vtest_y.append([0,1])
-vtest_x = np.asarray(vtest_x)
-vtest_y = np.asarray(vtest_y)
-
-
 # #### build model in keras ( a stacked LSTM model with many-to-one arch ) here 30 sequence and 2 output each for one category(m/f)
 
-# In[20]:
+# In[45]:
 
 
 #build the model: 2 stacked LSTM
-print('Build model...')
-input_bilstm=Input(shape = (maxlen,len_vocab))
-bi_one = Bidirectional(LSTM(512, return_sequences=True))(input_bilstm)
-drop1 = Dropout(0.2)(bi_one)
-bi_two = Bidirectional(LSTM(512, return_sequences=False))(drop1)
-drop2 = Dropout(0.2)(bi_two)
-output = Dense(2, activation='softmax')(drop2)
-model = Model(input_bilstm, output)
+# print('Build model...')
+# input_bilstm=Input(shape = (maxlen,len_vocab))
+# bi_one = Bidirectional(LSTM(512, return_sequences=True))(input_bilstm)
+# drop1 = Dropout(0.2)(bi_one)
+# bi_two = Bidirectional(LSTM(512, return_sequences=False))(drop1)
+# drop2 = Dropout(0.2)(bi_two)
+# output = Dense(2, activation='softmax')(drop2)
+# model = Model(input_bilstm, output)
 
 
-optimizer = optimizers.adam(lr = 0.01)
-model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+# optimizer = optimizers.adam(lr = 0.01)
+# model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
 
 
+# early_stopping = EarlyStopping(monitor='val_acc',patience=10, verbose=1)
+# model_checkpoint = ModelCheckpoint('Martin_program.hdf5',monitor='val_acc',save_best_only=True, verbose=1)
+# reduce_lr = ReduceLROnPlateau(monitor='val_acc',factor=0.5, patience=5, min_lr=0.0001, verbose=1)
+# print('Model Built')
+print("Weights are being imported from previously trained model..")
+model = load_model('Martin_program.hdf5')
+print("Weights imported")
 
-# In[25]:
+# batch_size=500
+# model.fit(train_X, train_Y,
+#           batch_size=batch_size,
+#           epochs=50,
+#           callbacks=[ model_checkpoint,reduce_lr,early_stopping],
+#           validation_data=(vtrain_x, vtrain_y),
+#           verbose = 2
+#          )
+# model.save('Martin_program.h5')
+# In[47]:
 
 
-batch_size=500
-model.fit(train_X, train_Y,
-          batch_size=batch_size,
-          epochs=50,
-          validation_data=(vtrain_x, vtrain_y)
-         )
-model.save('Martin_program.h5')
-
-
-# In[24]:
-
-
-score, acc = model.evaluate(vtest_x, vtest_y)
-print('Test score:', score)
+score, acc = model.evaluate(vtrain_x, vtrain_y)
 print('Test accuracy:', acc)
 
 
-# In[148]:
+# In[75]:
 
 
-name=["riya"]
+name=sys.argv[1]
+
+
+# name = ["Shivam"]
 X=[]
 trunc_name = [i[0:maxlen] for i in name]
 for i in trunc_name:
@@ -280,98 +257,16 @@ for i in trunc_name:
         tmp.append(set_flag(char_index["END"]))
     X.append(tmp)
 pred=model.predict(np.asarray(X))
-pred
+pred = pred.round()[0]
+if pred[0] == 1.0:
+    print( name[0], ".... is name of a male")
+else:
+    print(name[0], ".... is name of a female")
 
+#### Lets train more, clearly some very simple female names it doesnt get right like mentioned above (inspite it exists in training data)out = pd.DataFrame(prob_m)
+# out['name'] = test.name.reset_index()['name']
 
-# In[47]:
-
-
-pred
-
-
-# #### Lets train more, clearly some very simple female names it doesnt get right like mentioned above (inspite it exists in training data)
-
-# In[29]:
-
-
-score, acc = model.evaluate(vtest_x, vtest_y)
-print('Test score:', score)
-print('Test accuracy:', acc)
-
-
-# In[49]:
-
-
-name=["sandhya","jaspreet","rajesh","kaveri","aditi deepak","arihant","sasikala","aditi","ragini rajaram"]
-X=[]
-trunc_name = [i[0:maxlen] for i in name]
-for i in trunc_name:
-    tmp = [set_flag(char_index[j]) for j in str(i)]
-    for k in range(0,maxlen - len(str(i))):
-        tmp.append(set_flag(char_index["END"]))
-    X.append(tmp)
-pred=model.predict(np.asarray(X))
-pred
-
-
-# In[33]:
-
-
-name=["abhi","abhi deepak","mr. abhi"]
-X=[]
-trunc_name = [i[0:maxlen] for i in name]
-for i in trunc_name:
-    tmp = [set_flag(char_index[j]) for j in str(i)]
-    for k in range(0,maxlen - len(str(i))):
-        tmp.append(set_flag(char_index["END"]))
-    X.append(tmp)
-pred=model.predict(np.asarray(X))
-pred
-
-
-# In[34]:
-
-
-name=["rajini","rajinikanth","mr. rajini"]
-X=[]
-trunc_name = [i[0:maxlen] for i in name]
-for i in trunc_name:
-    tmp = [set_flag(char_index[j]) for j in str(i)]
-    for k in range(0,maxlen - len(str(i))):
-        tmp.append(set_flag(char_index["END"]))
-    X.append(tmp)
-pred=model.predict(np.asarray(X))
-pred
-
-
-# In[35]:
-
-
-#save our model and data
-model.save_weights('gender_model',overwrite=True)
-train.to_csv("train_split.csv")
-test.to_csv("test_split.csv")
-
-
-# In[36]:
-
-
-evals = model.predict(test_X)
-prob_m = [i[0] for i in evals]
-
-
-# In[37]:
-
-
-out = pd.DataFrame(prob_m)
-out['name'] = test.name.reset_index()['name']
-out['m_or_f']=test.m_or_f.reset_index()['m_or_f']
-
-
-# In[38]:
-
-
-out.head(10)
-out.columns = ['prob_m','name','actual']
-out.head(10)
-out.to_csv("gender_pred_out.csv")
+# out['m_or_f']=test.m_or_f.reset_index()['m_or_f']out.head(10)
+# out.columns = ['prob_m','name','actual']
+# out.head(10)
+# out.to_csv("gender_pred_out.csv")
